45ac9b632d8c1a06236de6a6d6d6de17c3e76823
Melissa Cuza <cuza001@csusm.edu>
2019-07-25 12:07:27 -0700

    Looping

---
+++ WeLearn/M2-javaScript/L3-JSobject-Array/Console/script.js
@@ -0,0 +1,46 @@
+const article = {
+  name: 'Dog family gives birth to 10 puppies',
+  views: 1035,
+  datePublished: '03/25/2018',
+  author: {
+    name: 'Joe Corgi',
+    title: 'Senior Canine Editor',
+  }
+};
+
+
+const glass = {
+  name: 'minecraft',
+  recipe: 'burn sand in a furnace',
+  color: 'clear',
+  coal: 8,
+};
+
+const food = {
+fruit: {
+  banana: 'peanut butter',
+  strawberry: 'chocolate',
+},
+mexican: {
+  tamales: 'verde',
+  gorditas: 'frijoles'
+},
+american: {
+  hamburgers: 'in n out',
+},
+};
+
+
+const seasons = ["Spring", "Summer", 'Fall', 'Winter'];
+const friends = ['Alice', 'Bob', 'Charlie'];
+const numbers = [5, 10, 2, 16, 50];
+
+const classes = [];
+classes.push({course: 'CS 111', title: 'Computer Sciened I'});
+classes.push({course: 'Math 160', title: "Calculus I"});
+
+const party = {
+  date; '7/4.2018',
+  location: 'Library',
+  guests: ['Alice','Bob', 'Charlie']
+};
