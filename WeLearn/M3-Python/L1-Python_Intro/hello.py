d66f078fb39ac82b72c3e41ad2b3d7b9c63a6e0a
Melissa Cuza <cuza001@csusm.edu>
2019-07-26 12:01:28 -0700

    python intro

---
+++ WeLearn/M3-Python/L1-Python_Intro/hello.py
@@ -0,0 +1,49 @@
+# print("Hello world!")
+# print("Bye world!")
+#
+#
+# num1 = int(raw_input("Enter number#1: "))
+# num2 = int(raw_input("Enter number#2: "))
+# total = num1 + num2
+# print("The sum is " + str(total))
+#
+#
+# num = int(raw_input("Enter a number: "))
+# if num > 0:
+#    print("That's a positive number!")
+# elif num < 0:
+#    print("That's a negative number!")
+# else:
+#    print("Zero is neither positive nor negative")
+#
+#
+# string = "hello there"
+# for letter in string:
+#    print(letter.upper())
+#
+# for i in range(5):
+#    print(i)
+#
+# x = 1
+# while x <= 5:
+#    print(x)
+#    x = x + 1
+#
+#
+# my_name = "Bob"
+# friend1 = "Alice"
+# friend2 = "John"
+# friend3 = "Mallory"
+#
+# print(
+# "My name is %s and my friends are %s, %s, %s" %
+# (my_name, friend1, friend2, friend3)
+# )
+#
+#
+# name = "Marina"
+# age = 19
+#
+# print("My name is " + name + " and I'm " + str(age) + " years old.")
+#
+# print("My name is %s and I'm %s years old." % (name, age))
